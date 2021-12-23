e6c504d2843fb74e61b12989687098e585c0d8ec
AAufdencamp <96022618+AAufdencamp@users.noreply.github.com>
2021-12-22 20:47:06 -0500

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,3 +1,13 @@
+# ------------------------------------------------------------------------
+# This is an exercise control flow with loops.
+#
+# This file is part of the Codeacademy learn Python project
+#
+# Student: Andrea Aufdencamp
+# Modified: 2021/12/22
+# See Readme for further details
+------------------------------------------------------------------------
+
 # Your code below:
 single_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
