561acd757396a3cbcd4df374fdd3d7af296bb3d4
AAufdencamp <96022618+AAufdencamp@users.noreply.github.com>
2021-12-26 20:59:49 -0500

    Update and rename Python_Code_Challenge_Lists_1.py.txt to Python_Code_Challenge_Lists_1.py

---
+++ Python_Code_Challenge_Lists_1.py
@@ -0,0 +1,17 @@
+# ------------------------------------------------------------------------
+# # Python Code Challenge Lists #1
+#
+# This file is part of the Codeacademy learn Python project
+#
+# Student: Andrea Aufdencamp
+# Modified: 2021/12/26
+# See Readme for further details
+------------------------------------------------------------------------
+
+def append_size(lst):
+   lst.append(len(lst))
+   return lst
+
+#Uncomment the line below when your function is done
+print(append_size([23, 42, 108]))
+
